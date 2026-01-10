e4407ea4a1979765492861a89e28afe476133597
Enzo Lanzellotti <enzo.maxpayne@gmail.com>
2026-01-10 17:54:23 -0300

    feat(artifactregistry): add repository format constants
    This creates a new types.go file to store the Repository Format constants
    (DOCKER, MAVEN, GO, KUBEFLOW, etc) to improve developer experience and
    avoid "magic strings".

---
+++ sdk/go/gcp/artifactregistry/types.go
@@ -0,0 +1,14 @@
+package artifactregistry
+
+// Repository Format constants for Artifact Registry
+const (
+	FormatDocker   = "DOCKER"
+	FormatMaven    = "MAVEN"
+	FormatNpm      = "NPM"
+	FormatPython   = "PYTHON"
+	FormatApt      = "APT"
+	FormatYum      = "YUM"
+	FormatKubeflow = "KUBEFLOW"
+	FormatGo       = "GO"
+	FormatGeneric  = "GENERIC"
+)
