b40491f89c149a61cfa1e72f1f38ec81d59f361d
Enzo Lanzellotti <enzo.maxpayne@gmail.com>
2026-01-10 23:40:52 -0300

    feat: add Location constants artifact registry

--- sdk/go/gcp/artifactregistry/types.go
+++ sdk/go/gcp/artifactregistry/types.go
@@ -12,3 +12,62 @@ const (
 	FormatGo       = "GO"
 	FormatGeneric  = "GENERIC"
 )
+
+// Location constants for Artifact Registry
+const (
+	// North America
+	LocationNorthAmericaNortheast1 = "northamerica-northeast1"
+	LocationNorthAmericaNortheast2 = "northamerica-northeast2"
+	LocationNorthAmericaSouth1     = "northamerica-south1"
+	LocationUsCentral1             = "us-central1"
+	LocationUsEast1                = "us-east1"
+	LocationUsEast4                = "us-east4"
+	LocationUsEast5                = "us-east5"
+	LocationUsSouth1               = "us-south1"
+	LocationUsWest1                = "us-west1"
+	LocationUsWest2                = "us-west2"
+	LocationUsWest3                = "us-west3"
+	LocationUsWest4                = "us-west4"
+
+	// South America
+	LocationSouthAmericaEast1 = "southamerica-east1"
+	LocationSouthAmericaWest1 = "southamerica-west1"
+
+	// Europe
+	LocationEuropeCentral2   = "europe-central2"
+	LocationEuropeNorth1     = "europe-north1"
+	LocationEuropeNorth2     = "europe-north2"
+	LocationEuropeSouthwest1 = "europe-southwest1"
+	LocationEuropeWest1      = "europe-west1"
+	LocationEuropeWest2      = "europe-west2"
+	LocationEuropeWest3      = "europe-west3"
+	LocationEuropeWest4      = "europe-west4"
+	LocationEuropeWest6      = "europe-west6"
+	LocationEuropeWest8      = "europe-west8"
+	LocationEuropeWest9      = "europe-west9"
+	LocationEuropeWest10     = "europe-west10"
+	LocationEuropeWest12     = "europe-west12"
+
+	// Middle East
+	LocationMeCentral1 = "me-central1"
+	LocationMeCentral2 = "me-central2"
+	LocationMeWest1    = "me-west1"
+
+	// Asia
+	LocationAsiaEast1      = "asia-east1"
+	LocationAsiaEast2      = "asia-east2"
+	LocationAsiaNortheast1 = "asia-northeast1"
+	LocationAsiaNortheast2 = "asia-northeast2"
+	LocationAsiaNortheast3 = "asia-northeast3"
+	LocationAsiaSouth1     = "asia-south1"
+	LocationAsiaSouth2     = "asia-south2"
+	LocationAsiaSoutheast1 = "asia-southeast1"
+	LocationAsiaSoutheast2 = "asia-southeast2"
+
+	// Australia
+	LocationAustraliaSoutheast1 = "australia-southeast1"
+	LocationAustraliaSoutheast2 = "australia-southeast2"
+
+	// Africa
+	LocationAfricaSouth1 = "africa-south1"
+)
